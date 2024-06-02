64d707a7b4795f1fe48513bfbe59a9e0bb2d967b
Bruno Souza <bruno.souza@crdc.com.br>
2024-06-02 13:27:33 -0300

    Add Model class for account

---
+++ payflow/src/main/java/com/brunosouza/payflow/domain/model/Account.java
@@ -0,0 +1,34 @@
+package com.brunosouza.payflow.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate dueDate;
+
+    private LocalDate paymentDate;
+
+    private BigDecimal value;
+
+    private String description;
+
+    private String status;
+
+}
